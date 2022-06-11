044b2e333e347d0e0e9c18cedc541cb97c0ae045
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-06-11 16:42:44 +0900

    バリデーション、ユニーク制約作成、セキュリティパスワード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :blogs
+  has_many :favorites, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, uniqueness: true
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
